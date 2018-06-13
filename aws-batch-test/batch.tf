eeccb548a299a2b8896b185d5c683526ff72122a
Marc Monnerat <procrastinatio@gmail.com>
2018-06-13 16:36:09 +0000

    working

--- aws-batch-test/batch.tf
+++ aws-batch-test/batch.tf
@@ -1,42 +1,77 @@
 
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+        "Service": "ec2.amazonaws.com"
+        }
+    }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = "${aws_iam_role.ecs_instance_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_role" {
+  name  = "ecs_instance_role"
+  role = "${aws_iam_role.ecs_instance_role.name}"
+}
+
+resource "aws_iam_role" "aws_batch_service_role" {
+  name = "aws_batch_service_role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+        "Service": "batch.amazonaws.com"
+        }
+    }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
+  role       = "${aws_iam_role.aws_batch_service_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
+
 resource "aws_security_group" "sample" {
   name = "aws_batch_compute_environment_security_group"
 }
 
 resource "aws_vpc" "sample" {
   cidr_block = "10.1.0.0/16"
-  tags {
-    Name = "aws_batch_demo"
-  }
 }
 
 resource "aws_subnet" "sample" {
   vpc_id = "${aws_vpc.sample.id}"
   cidr_block = "10.1.1.0/24"
-  tags {
-    Name = "aws_batch_demo"
-  }
-}
-
-
-resource "aws_batch_job_queue" "test_queue" {
-  name = "tf-test-batch-job-queue"
-  state = "ENABLED"
-  priority = 1
-  compute_environments = ["${aws_batch_compute_environment.sample.arn}"]
-
 }
 
 resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "mom_sample"
+  compute_environment_name = "sample"
   compute_resources {
     instance_role = "${aws_iam_instance_profile.ecs_instance_role.arn}"
     instance_type = [
-      "m4.large",
+      "c4.large",
     ]
-    max_vcpus = 4
+    max_vcpus = 16
     min_vcpus = 0
-    desired_vcpus = 2
     security_group_ids = [
       "${aws_security_group.sample.id}"
     ]
@@ -49,53 +84,3 @@ resource "aws_batch_compute_environment" "sample" {
   type = "MANAGED"
   depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
 }
-
-
-resource "aws_batch_job_definition" "test" {
-    name = "tf_test_batch_job_definition"
-    type = "container"
-    container_properties = <<CONTAINER_PROPERTIES
-{
-    "command": ["ls", "-la"],
-    "image": "busybox",
-    "memory": 1024,
-    "vcpus": 1,
-"volumes": [
-{
-"host": {
-          "sourcePath": "/tmp"
-        
-},
-        "name": "tmp"
-      
-}
-    
-],
-"environment": [
-        {"name": "VARNAME", "value": "VARVAL"}
-    
-],
-"mountPoints": [
-{
-          "sourceVolume": "tmp",
-          "containerPath": "/tmp",
-          "readOnly": false
-        
-}
-    
-],
-"ulimits": [
-{
-        "hardLimit": 1024,
-        "name": "nofile",
-        "softLimit": 1024
-      
-}
-    
-]
-
-}
-CONTAINER_PROPERTIES
-
-}
-
