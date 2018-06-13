490937f4817804ec5807a9673bc376e0f003fe5e
Marc Monnerat <procrastinatio@gmail.com>
2018-06-13 16:34:44 +0000

    issue

--- aws-batch-test/batch.tf
+++ aws-batch-test/batch.tf
@@ -1,6 +1,10 @@
 
 resource "aws_security_group" "sample" {
   name = "aws_batch_compute_environment_security_group"
+  vpc_id   = "${aws_vpc.sample.id}"
+  tags {
+      Name = "aws_batch_demo"
+  }
 }
 
 resource "aws_vpc" "sample" {
@@ -18,62 +22,64 @@ resource "aws_subnet" "sample" {
   }
 }
 
+resource "aws_batch_compute_environment" "sample" {
+  compute_environment_name = "aws_batch_demo"
+   compute_resources {
+     instance_role = "${aws_iam_instance_profile.ecs_instance_role.arn}"
+     instance_type = [
+       "optimal"
+     ]
+     max_vcpus = 1024
+     min_vcpus = 0
+     desired_vcpus = 2
+     security_group_ids = [
+       "${aws_security_group.sample.id}"
+     ]
+     subnets = [
+       "${aws_subnet.sample.id}"
+     ]
+     type = "EC2"
+   }
+  service_role = "${aws_iam_role.aws_batch_service_role.arn}"
+  type = "MANAGED"
+  depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
+}
 
 resource "aws_batch_job_queue" "test_queue" {
   name = "tf-test-batch-job-queue"
   state = "ENABLED"
   priority = 1
   compute_environments = ["${aws_batch_compute_environment.sample.arn}"]
-
 }
-
-resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "mom_sample"
-  compute_resources {
-    instance_role = "${aws_iam_instance_profile.ecs_instance_role.arn}"
-    instance_type = [
-      "m4.large",
-    ]
-    max_vcpus = 4
-    min_vcpus = 0
-    desired_vcpus = 2
-    security_group_ids = [
-      "${aws_security_group.sample.id}"
-    ]
-    subnets = [
-      "${aws_subnet.sample.id}"
-    ]
-    type = "EC2"
-  }
-  service_role = "${aws_iam_role.aws_batch_service_role.arn}"
-  type = "MANAGED"
-  depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
-}
-
 
 resource "aws_batch_job_definition" "test" {
     name = "tf_test_batch_job_definition"
     type = "container"
     container_properties = <<CONTAINER_PROPERTIES
 {
-    "command": ["ls", "-la"],
-    "image": "busybox",
-    "memory": 1024,
+    "command": ["myjob.sh"],
+    "image": "procrastinatio/aws-batch",
+    "memory": 256,
     "vcpus": 1,
-"volumes": [
-{
-"host": {
+    "volumes": [
+      {
+        "host": {
           "sourcePath": "/tmp"
-        
-},
+        },
         "name": "tmp"
-      
-}
+      }
     
-],
-"environment": [
-        {"name": "VARNAME", "value": "VARVAL"}
-    
+    ],
+    "environment": [
+            {   
+                "name": "BATCH_FILE_S3_URL",
+                "value": "s3://ltmom-aws-batch-test/mapjob.sh"
+            },  
+            {   
+                "name": "BATCH_FILE_TYPE",
+                "value": "script"
+            }   
+        
 ],
 "mountPoints": [
 {
@@ -83,14 +89,6 @@ resource "aws_batch_job_definition" "test" {
         
 }
     
-],
-"ulimits": [
-{
-        "hardLimit": 1024,
-        "name": "nofile",
-        "softLimit": 1024
-      
-}
     
 ]
 
--- aws-batch-test/iam.tf
+++ aws-batch-test/iam.tf
@@ -21,6 +21,12 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
   role       = "${aws_iam_role.ecs_instance_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+# New
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = ["${aws_iam_role.ecs_instance_role.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
 
 resource "aws_iam_instance_profile" "ecs_instance_role" {
   name  = "ecs_instance_role_for_batch"
@@ -50,4 +56,31 @@ resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
+# ---- IAM for S3 access -------------
 
+
+resource "aws_iam_role" "s3-test-role" {
+  name               = "aws-batch-test-role"
+  assume_role_policy = "${file("assume-role-policy.json")}"
+
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "aws-batch-test-policy"
+  description = "AWS Batch S3 test policy"
+  policy      = "${file("policy-s3-bucket.json")}"
+
+}
+
+# resource "aws_iam_policy_attachment" "test-attach" {
+#   name       = "test-attachment"
+#   roles      = ["${aws_iam_role.s3-test-role.name}"]
+#   policy_arn = "${aws_iam_policy.policy.arn}"
+# 
+# }
+# 
+# resource "aws_iam_instance_profile" "test_profile" {
+#   name  = "batch_profile"
+#   roles = ["${aws_iam_role.s3-test-role.name}"]
+# 
+#}
